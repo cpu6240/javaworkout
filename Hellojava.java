0118dac5a99b9bad176f009a24abb61cee5a0cbc
CPU <121150904+cpu6240@users.noreply.github.com>
2023-03-30 15:27:33 +0900

    Add files via upload
    이클립스 설치 후 처음 파일 만들기

---
+++ Hellojava.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello, Java"); 한 줄 주석 연습입니다.
+		/*
+		 Date : 2023년 03월 30일
+		 Author : 강지수
+		 Description : 첫 번째 자바 프로그램입니다.
+		 Version : 1.0
+		*/
+		System.out.println("Hello, Java");
+	}
+
+}
