b517e2369393405281225fb4a5acb331173fdc6d
CPU <121150904+cpu6240@users.noreply.github.com>
2023-03-30 16:11:54 +0900

    문자형 변수 + 이클립스 한글 세팅
    Run - Run Configurations - Common - Encoding - MS949 setting

---
+++ KoreanTest.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class KoreanTest {
+	public static void main(String[] args) {
+		String a1 = "한글";
+		String b1 = "테스트";
+		
+		System.out.println(a1);
+		System.out.println(b1);
+		
+	}
+
+}
