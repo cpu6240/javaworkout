a6f3e032756ec048efe895dab171d4d8491fedbd
CPU <121150904+cpu6240@users.noreply.github.com>
2023-04-03 16:05:41 +0900

    2장 예제

---
+++ ch2_ex4.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class ch2_ex4 {
+	public static void main(String[] args) {
+		int a = 10;
+		double b = 2.0;
+		
+		int c = a+(int)b;
+		
+		System.out.println(c);
+	}
+
+}
---
+++ ch2_ex5.java
@@ -0,0 +1,10 @@
+package chapter2;
+
+public class ch2_ex5 {
+	public static void main(String[] args) {
+		char a = '\uAE00';
+		
+		System.out.println(a);
+	}
+
+}
